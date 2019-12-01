fbde7e66ed8b4b4109a84c9ce3ea45a02895e4c8
Robert <robert.damoc@yahoo.com>
2019-12-01 22:16:59 +0200

    [2019] New Year, New Adventures. Day 1

---
+++ 2019/day1.rb
@@ -0,0 +1,28 @@
+require 'basic_file_reader'
+
+def compute_fuel(mass)
+  fuel = mass / 3 - 2
+  fuel < 0 ? 0 : fuel
+end
+
+# Task 1
+fuel = BasicFileReader.lines(file_name: 'inputs/day1')
+                      .map { |nr| compute_fuel(nr.to_i) }
+                      .reduce(:+)
+
+puts fuel # 3147032
+
+# Task 2
+fuel = BasicFileReader.lines(file_name: 'inputs/day1') do |nr|
+  fuel = compute_fuel(nr.to_i)
+  total_fuel = fuel
+
+  while fuel > 0
+    fuel = compute_fuel(fuel)
+    total_fuel += fuel
+  end
+
+  total_fuel
+end.reduce(:+)
+
+puts fuel # 4717699
